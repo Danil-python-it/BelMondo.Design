657780390e9ce02ce20ebb6b2a7b9b7b2f98c926
Dodo birds <81391324+Danil-python-it@users.noreply.github.com>
2022-04-05 21:19:37 +0300

    Add files via upload

---
+++ scripts-js-php/sendemail.php
@@ -0,0 +1,23 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'path/to/PHPMailer/src/Exception.php';
+require 'path/to/PHPMailer/src/PHPMailer.php';
+
+$mail = new PHPMailer(true);
+$mail->setLanguage("ru");
+$mail->From = "Veru@mail.ru";
+$mail->FromName = "FIFIFIFI";
+$mail->isHTML(true);
+$mail->addAddress("Davolk2007@mail.ru", "LLLLL");
+$mail->Subject = "Subject Text";
+$mail->Body = "<i>Mail body in HTML</i>";
+$mail->AltBody = "This is the plain text version of the email content";
+
+try {
+    $mail->send();
+    echo "Message has been sent successfully";
+} catch (Exception $e) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
+}
